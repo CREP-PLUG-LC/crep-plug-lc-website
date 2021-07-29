890076d5aba8368ae02c6df0b3a9e7186c762a76
Hamza <rafi.hamza2020@gmail.com>
2021-07-29 03:44:15 +0100

    Added multiple pages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+// align outer circles in a circle around the middle circles
 function updateCirclePosition(){
-    var circles = document.getElementsByClassName('outerCircle')
+    var circles = document.querySelector('.currentPage').children
     
     // six items
     var theta = [0, Math.PI / 3, (Math.PI * 2)/ 3, Math.PI, (Math.PI * 4) / 3, (Math.PI * 5) / 3]
@@ -22,10 +23,10 @@ function updateCirclePosition(){
     }
 }
 
-window.onresize = updateCirclePosition
-window.onload = updateCirclePosition
+// window.onresize = updateCirclePosition()
+window.onload = updateCirclePosition()
 
-
+// Set modal information for each shoe
 var circles = document.getElementsByClassName('outerCircle')
 for(i = 0; i < circles.length; i++){
     circles[i].onclick = (event) => {
@@ -54,10 +55,61 @@ for(i = 0; i < circles.length; i++){
     }
 }
 
+// Click outside modal to close
 window.onclick = event => {
     let modal = document.getElementById('modal')
 
     if (event.target == modal){
         modal.style.display = 'none'
     }
+}
+
+// buttons cause the 'page' to change
+var leftButton = document.getElementById('leftButton')
+var rightButton = document.getElementById('rightButton')
+
+var pageOrder = ['leftPage', 'mainPage', 'rightPage']
+
+function scrollPage(direction){    
+    let currentPage = document.querySelector('.currentPage')
+    
+    // Get current index of page 
+    let currentPageIndex = 0
+    for(i = 0; i < pageOrder.length; i++){
+        if(currentPage.classList.contains(pageOrder[i])){
+            currentPageIndex = i
+        }
+    }
+
+    pageIndexToScrollTo = -1
+
+    if(direction === "left"){
+        if(currentPageIndex !== 0){
+            var pageIndexToScrollTo = currentPageIndex - 1
+        }
+    }
+    else if(direction === "right"){
+        if(currentPageIndex !== 2){
+            var pageIndexToScrollTo = currentPageIndex + 1
+        }
+    }
+
+    if(pageIndexToScrollTo !== -1){
+        currentPage.classList.remove('currentPage')
+        currentPage.classList.add('hiddenPage')
+        
+        var pageToScrollTo = document.querySelector('.' + pageOrder[pageIndexToScrollTo])
+
+        pageToScrollTo.classList.remove('hiddenPage')
+        pageToScrollTo.classList.add('currentPage')
+        updateCirclePosition()
+    }
+}
+
+leftButton.onclick = () => {
+    scrollPage('left')
+}
+
+rightButton.onclick = () => {
+    scrollPage('right')
 }
